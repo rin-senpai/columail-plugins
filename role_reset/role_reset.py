0a13e0d4da720a756afcdb5c1ddd25f02d15566c
りん <31785428+rin-senpai@users.noreply.github.com>
2022-09-04 10:48:16 +0800

    Create role_reset.py

---
+++ role_reset/role_reset.py
@@ -0,0 +1,28 @@
+from discord.ext import commands
+from discord.utils import get
+
+from core import checks
+from core.models import PermissionLevel
+
+class RoleReset(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
+    async def lmao(ctx):
+        role_names = ('Abyss Herrscher')
+        roles = tuple(get(ctx.guild.roles, name=name) for name in role_names)
+        for member in ctx.guild.members:
+            try:
+                await member.remove_roles(*roles)
+            except:
+                print(f'Couldn\'t remove roles from {member}')
+        await ctx.send('deadded')
+
+    @commands.command()
+    async def test(ctx):
+        await ctx.send('woah it works')
+
+def setup(bot):
+    bot.add_cog(RoleReset(bot))
